19407a3ae2655b5c9207e60ee7306cc78ba1d660
Vitaly Lakhtin <lvvlvv11@gmail.com>
2017-12-20 01:35:08 +0200

    2 lab

---
+++ lab2/lakhtin/lab2/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+  currentNumb(num: number){
+    num % 2 === 0 ? this.evenNumbers.push(num)
+    : this.oddNumbers.push(num);
+  }
+}
